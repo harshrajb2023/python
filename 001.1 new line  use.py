46d14f52d51702531952367912085b432aaae22d
Harsh Raj Bind <121737517+harshrajb2023@users.noreply.github.com>
2026-01-29 14:35:39 +0530

    Update 001.1 new line  use.py

--- 001.1 new line  use.py
+++ 001.1 new line  use.py
@@ -24,6 +24,14 @@ with open("example.txt", "w") as file:
 
 
 
+#output
+Explanation (quick & clear):
+"w" → write mode (creates file if it doesn’t exist, clears it if it does)
+\n → new line
+with open(...) as file: → automatically closes the file after writing
+
+
+
 
 lines = ["Line 1", "Line 2", "Line 3"]
 result = "\n".join(lines)
@@ -31,12 +39,30 @@ print(result)
 
 
 
+#Output
+Line 1
+Line 2
+Line 3
+
 
 
 
 for i in range(1, 4):
     print("Line {i}\n", end="")  # Avoids extra newlines
+
+# wrong
+Why?
+"Line {i}" is just a normal string
+Python does not replace {i} unless you tell it to
+{i} is printed literally
+
+    
        #vs
  
 for i in range(1, 4):
     print(f"Line {i}\n", end="")  # Avoids extra newlines
+
+# currect
+Why?
+f"..." is an f-string
+{i} gets replaced with the value of i
